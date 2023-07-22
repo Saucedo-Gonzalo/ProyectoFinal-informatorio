194f12aa9b992c2a7cd60764a5981d47d3133c79
Saucedo-Gonzalo <gonzalosaucedoo15@gmail.com>
2023-07-22 09:49:51 -0300

    comente las url para correr servidor

--- repositorio/raiz/apps/noticias/urls.py
+++ repositorio/raiz/apps/noticias/urls.py
@@ -5,23 +5,23 @@ from . import views
 app_name='noticias'
 
 urlpatterns = [
-    #CRUD NOTICIAS
+    # #CRUD NOTICIAS
 
-    #create
-    path('agregar/', views.agregarNoticia, name='agregarNoticia'),
+    # #create
+    # path('agregar/', views.agregarNoticia, name='agregarNoticia'),
 
-    #read
-    path('listar/', views.listarNoticias, name='listarNoticias'),
-    path('listarPorFecha/', views.listarPorFecha, name='listarPorFecha'),
-    path('listarPorObjetivo/<int:pk>', views.listarPorObjetivo, name='listarPorObjetivo'),
-    path('Mostrar/<int:pk>/', views.detalleNoticia.as_view(), name='detalleNoticia'),
+    # #read
+    # path('listar/', views.listarNoticias, name='listarNoticias'),
+    # path('listarPorFecha/', views.listarPorFecha, name='listarPorFecha'),
+    # path('listarPorObjetivo/<int:pk>', views.listarPorObjetivo, name='listarPorObjetivo'),
+    # path('Mostrar/<int:pk>/', views.detalleNoticia.as_view(), name='detalleNoticia'),
 
-    #update
-    path('modificarNoticia/<int:pk>', views.modificarNoticia, name='modificarNoticia'),
-    path('modificarComentario/<int:pk>', views.modificarComentario, name='modificarComentario'),
+    # #update
+    # path('modificarNoticia/<int:pk>', views.modificarNoticia, name='modificarNoticia'),
+    # path('modificarComentario/<int:pk>', views.modificarComentario, name='modificarComentario'),
 
-    #delete
-    path('eliminarNoticia/<int:pk>', views.eliminarNoticia, name='eliminarNoticia'),
-    path('eliminarComentario/<int:pk>', views.eliminarComentario, name='eliminarComentario'),
+    # #delete
+    # path('eliminarNoticia/<int:pk>', views.eliminarNoticia, name='eliminarNoticia'),
+    # path('eliminarComentario/<int:pk>', views.eliminarComentario, name='eliminarComentario'),
 
 ]
